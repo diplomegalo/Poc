3a40d9c899e7b34c4960b4f4fccef5c53b358410
DELSAUT Pierre-arnaud <dlsprr@forem.be>
2022-09-19 13:37:23 +0200

    create all projects

---
+++ Poc.GraphQL.Gtw/Poc.GraphQL.Menu/Query.cs
@@ -0,0 +1,17 @@
+﻿namespace Poc.GraphQL.Menu
+{
+    public class Query
+    {
+        public Person GetPerson() => new Person("Luke Skywalker");
+    }
+
+    public class Person
+    {
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+}
